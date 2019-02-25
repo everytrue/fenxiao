e98f48fc997299293ff296416b49470ca40e57f4
qinshuze <qinshuze@hengqijy.com>
2019-02-25 11:32:39 +0800

    提交修改

--- application/admin/view/approval/approval/index.php
+++ application/admin/view/approval/approval/index.php
@@ -1,82 +1,8 @@
 {extend name="public/container"}
 {block name="content"}
 <div class="layui-fluid">
-    <div class="layui-row layui-col-space15"  id="app">
-        <!--搜索条件-->
-        <div class="layui-col-md12">
-            <div class="layui-card">
-                <div class="layui-card-header">搜索条件</div>
-                <div class="layui-card-body">
-                    <div class="layui-carousel layadmin-carousel layadmin-shortcut" lay-anim="" lay-indicator="inside" lay-arrow="none" style="background:none">
-                        <div class="layui-card-body">
-                            <div class="layui-row layui-col-space10 layui-form-item">
-                                <div class="layui-col-lg12">
-                                    <label class="layui-form-label">订单状态:</label>
-                                    <div class="layui-input-block" v-cloak="">
-                                        <button class="layui-btn layui-btn-sm" :class="{'layui-btn-primary':where.status!==item.value}" @click="where.status = item.value" type="button" v-for="item in orderStatus">{{item.name}}
-                                            <span v-if="item.count!=undefined" :class="item.class!=undefined ? 'layui-badge': 'layui-badge layui-bg-gray' ">{{item.count}}</span></button>
-                                    </div>
-                                </div>
-                                <div class="layui-col-lg12">
-                                    <label class="layui-form-label">订单类型:</label>
-                                    <div class="layui-input-block" v-cloak="">
-                                        <button class="layui-btn layui-btn-sm" :class="{'layui-btn-primary':where.type!=item.value}" @click="where.type = item.value" type="button" v-for="item in orderType">{{item.name}}
-                                            <span v-if="item.count!=undefined" class="layui-badge layui-bg-gray">{{item.count}}</span></button>
-                                    </div>
-                                </div>
-                                <div class="layui-col-lg12">
-                                    <label class="layui-form-label">创建时间:</label>
-                                    <div class="layui-input-block" data-type="data" v-cloak="">
-                                        <button class="layui-btn layui-btn-sm" type="button" v-for="item in dataList" @click="setData(item)" :class="{'layui-btn-primary':where.data!=item.value}">{{item.name}}</button>
-                                        <button class="layui-btn layui-btn-sm" type="button" ref="time" @click="setData({value:'zd',is_zd:true})" :class="{'layui-btn-primary':where.data!='zd'}">自定义</button>
-                                        <button type="button" class="layui-btn layui-btn-sm layui-btn-primary" v-show="showtime==true" ref="date_time">{$year.0} - {$year.1}</button>
-                                    </div>
-                                </div>
-                                <div class="layui-col-lg12">
-                                    <label class="layui-form-label">订单号:</label>
-                                    <div class="layui-input-block">
-                                        <input type="text" name="real_name" style="width: 50%" v-model="where.real_name" placeholder="请输入姓名、电话、订单编号" class="layui-input">
-                                    </div>
-                                </div>
-                                <div class="layui-col-lg12">
-                                    <div class="layui-input-block">
-                                        <button @click="search" type="button" class="layui-btn layui-btn-sm layui-btn-normal">
-                                            <i class="layui-icon layui-icon-search"></i>搜索</button>
-                                        <button @click="excel" type="button" class="layui-btn layui-btn-warm layui-btn-sm export" type="button">
-                                            <i class="fa fa-floppy-o" style="margin-right: 3px;"></i>导出</button>
-                                        <button @click="refresh" type="reset" class="layui-btn layui-btn-primary layui-btn-sm">
-                                            <i class="layui-icon layui-icon-refresh" ></i>刷新</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--end-->
-        <!-- 中间详细信息-->
-        <div :class="item.col!=undefined ? 'layui-col-sm'+item.col+' '+'layui-col-md'+item.col:'layui-col-sm6 layui-col-md3'" v-for="item in badge" v-cloak="" v-if="item.count > 0">
-            <div class="layui-card">
-                <div class="layui-card-header">
-                    {{item.name}}
-                    <span class="layui-badge layuiadmin-badge" :class="item.background_color">{{item.field}}</span>
-                </div>
-                <div class="layui-card-body">
-                    <p class="layuiadmin-big-font">{{item.count}}</p>
-                    <p v-show="item.content!=undefined">
-                        {{item.content}}
-                        <span class="layuiadmin-span-color">{{item.sum}}<i :class="item.class"></i></span>
-                    </p>
-                </div>
-            </div>
-        </div>
-        <!--enb-->
-    </div>
     <!--列表-->
-    <table id="list" lay-filter="list">
-
-    </table>
+    <table id="list" lay-filter="list"></table>
     <!--end-->
 </div>
 <script src="{__ADMIN_PATH}js/layuiList.js"></script>
