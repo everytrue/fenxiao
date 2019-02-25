624c91796a1432bde155e65a63e24769121abf59
奔跑的咸鱼 <1191249254@qq.com>
2019-02-25 13:07:43 +0800

    更新代码

--- application/admin/view/approval/approval/detail.php
+++ application/admin/view/approval/approval/detail.php
@@ -6,19 +6,19 @@
             </div>
             <div class="panel-body">
                 <div class="row show-grid">
-                    <div class="col-xs-6">订单ID: {approval.id}</div>
-                    <div class="col-xs-6">用户ID: {approval.uid}</div>
-                    <div class="col-xs-6">订单编号: {approval.order_id}</div>
-                    <div class="col-xs-6">姓名: {approval.name}</div>
-                    <div class="col-xs-6">订单状态: {approval.status}</div>
-                    <div class="col-xs-6">手机号码: {approval.phone}</div>
-                    <div class="col-xs-6">批注: {approval.remark}</div>
-                    <div class="col-xs-6">身份证号码: {approval.id_number}</div>
-                    <div class="col-xs-6">创建时间: {approval.create_time}</div>
-                    <div class="col-xs-6">推广经验: {approval.experience}</div>
-                    <div class="col-xs-6">审批人: {approval.approver}</div>
-                    <div class="col-xs-6">申请银行: {approval.bank}</div>
-                    <div class="col-xs-6">审批时间: {approval.approval_time}</div>
+                    <div class="col-xs-6">订单ID: {$approval.id}</div>
+                    <div class="col-xs-6">用户ID: {$approval.uid}</div>
+                    <div class="col-xs-6">订单编号: {$approval.order_id}</div>
+                    <div class="col-xs-6">姓名: {$approval.name}</div>
+                    <div class="col-xs-6">订单状态: {$approval.status}</div>
+                    <div class="col-xs-6">手机号码: {$approval.phone}</div>
+                    <div class="col-xs-6">批注: {$approval.remark}</div>
+                    <div class="col-xs-6">身份证号码: {$approval.id_number}</div>
+                    <div class="col-xs-6">创建时间: {$approval.create_time}</div>
+                    <div class="col-xs-6">推广经验: {$approval.experience}</div>
+                    <div class="col-xs-6">审批人: {$approval.approver}</div>
+                    <div class="col-xs-6">申请银行: {$approval.bank}</div>
+                    <div class="col-xs-6">审批时间: {$approval.approval_time}</div>
                 </div>
             </div>
         </div>
